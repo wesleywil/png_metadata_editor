20ace213db4cda90440e2f3ef472921f01b55bf4
wesleywil <wesleywilsonti@gmail.com>
2024-02-12 09:36:28 -0300

    updated scripts.js by making use of all the data received from the upload_img rust function, and tied things up in the homepage and css styles

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -1,75 +1,40 @@
 
-const { invoke } = window.__TAURI__.tauri
+const { invoke, convertFileSrc } = window.__TAURI__.tauri
 const { Body } = window.__TAURI__.http;
 const { open } = window.__TAURI__.dialog;
 
 document.addEventListener("DOMContentLoaded", function () {
     // Show uploaded image
-    const upload = document.getElementById("png_file");
-    const container = document.getElementById("to_remove");
-
-    // upload.addEventListener("change", () => {
-    //     const selectedFile = upload.files[0];
-    //     const reader = new FileReader();
-    //     reader.onload = function (event) {
-    //         const imageURL = event.target.result;
-    //         const img = document.createElement("img");
-    //         img.src = imageURL;
-    //         img.className = "upload_img"
-    //         container.appendChild(img);
-    //     }
-    //     reader.readAsDataURL(selectedFile)
-    //     console.log(selectedFile);
-
-    //     // Trying to upload image
-    //     // const form = new FormData();
-    //     // form.append('key', 'value');
-    //     // form.append('image', selectedFile);
-    //     // const formBody = Body.form(form);
-
-
-    //     // const test_upload_rust = async () => {
-    //     //     try {
-    //     //         const openFile = await open({
-    //     //             multiple: false,
-    //     //             filters: [{
-    //     //                 name: "test_test_img",
-    //     //                 extensions: ['png']
-    //     //             }]
-    //     //         });
-    //     //         await invoke("upload_img_test", openFile)
-    //     //     } catch (error) {
-    //     //         console.log("ERROR ", error);
-    //     //     }
-    //     // }
-    //     // test_upload_rust();
-    // });
+    const imgContainer = document.getElementById("img_container");
+    const imgTitle = document.getElementById("title");
     const imgParams = document.getElementById("text_params");
     const dataParams = document.getElementById("generation_data_params");
-    // Rust tauri tests
-
-    // invoke('read_img_test')
-    //     .then((response) => {
-    //         console.log('RESPONSE FROM INVOKE READ IMG TEST ---> ', response)
-    //         imgParams.value = response[0];
-    //         dataParams.value = response[1]
-    //     })
-    //     .catch((error) => {
-    //         console.log('ERROR ---> ', error);
-    //     })
-
     const uploadImage = document.getElementById("btn_upload_img");
 
     uploadImage.addEventListener("click", async () => {
+        const removeImg = document.getElementById("img_upload_ig");
+        if (removeImg) {
+            removeImg.remove();
+        }
+
         try {
             const openFile = await open({
                 multiple: false,
                 filters: [{
-                    name: "test_test_img",
+                    name: "img",
                     extensions: ['png']
                 }]
             });
-            await invoke("upload_img_test", { file: openFile });
+            const res = await invoke("upload_img", { file: openFile });
+            const img = document.createElement("img");
+            img.id = "img_upload_ig";
+            img.src = convertFileSrc(openFile)
+            img.className = "img_upload";
+            imgContainer.appendChild(img);
+            imgTitle.value = res[0].replace(/^.*[\\\/]/, '');
+            imgParams.value = res[1];
+            dataParams.value = res[2];
+
         } catch (err) {
             console.log('Error: ----> ', err);
         }
