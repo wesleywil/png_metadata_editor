6ab20067961432fa9a33653eb31f405edd2e8ac8
wesleywil <wesleywilsonti@gmail.com>
2024-02-12 09:34:11 -0300

    removed test functions,updated Error enum by removing not used dependencies, and updated the main function that allows users to upload a image to be decoded

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,15 +1,8 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
-#[tauri::command]
-fn greet(name: &str) -> String {
-    format!("Hello, {}!", name)
-}
-
 #[derive(Debug, thiserror::Error)]
 enum Error {
-    #[error(transparent)] Exif(#[from] exif::Error),
-    #[error(transparent)] ImgParts(#[from] img_parts::Error),
     #[error(transparent)] Png(#[from] png::DecodingError),
     #[error(transparent)] Io(#[from] std::io::Error),
 }
@@ -22,39 +15,28 @@ impl serde::Serialize for Error {
 }
 
 #[tauri::command]
-fn read_img_test() -> Result<Vec<String>, Error> {
+fn upload_img(file: String) -> Result<Vec<String>, Error> {
     let mut formatted_strings = Vec::new();
-    let img_path =
-        "C:/Users/wesle/OneDrive/Área de Trabalho/Projects/edit_png/src-tauri/src/test_img/to_test.png";
-    let decoder = png::Decoder::new(std::fs::File::open(img_path).unwrap());
-    let mut reader = decoder.read_info().unwrap();
-    // println!("Reader: {}", reader);
-    let mut buf = vec![0; reader.output_buffer_size()];
-    let info = reader.next_frame(&mut buf).unwrap();
-    println!("Info: {:?}", info);
-    formatted_strings.push(reader.info().uncompressed_latin1_text[0].text.clone());
-    formatted_strings.push(reader.info().uncompressed_latin1_text[1].text.clone());
-    // for text_chunk in &reader.info().uncompressed_latin1_text {
-    //     println!("Keyword: {:?}", text_chunk.keyword);
-    //     println!("Text Chunk: {:?}", text_chunk);
-    // }
+    let decoder = png::Decoder::new(std::fs::File::open(file.clone()).unwrap());
+    let reader = decoder.read_info().unwrap();
+    formatted_strings.push(file);
+    if reader.info().uncompressed_latin1_text.len() > 0 {
+        formatted_strings.push(reader.info().uncompressed_latin1_text[0].text.clone());
+        if reader.info().uncompressed_latin1_text.len() > 1 {
+            formatted_strings.push(reader.info().uncompressed_latin1_text[1].text.clone());
+        } else {
+            formatted_strings.push("no parameters extra data".to_string());
+        }
+    } else {
+        println!("No elements exist");
+    }
     Ok(formatted_strings)
-}
-
-use tauri::http::Request;
-use serde_json::Value;
-
-#[tauri::command]
-fn upload_img_test(file: String) -> Result<String, Error> {
-    println!("THAT'S IT? {}", file);
-
-    Ok("maybe?".to_string())
 }
 
 fn main() {
     tauri::Builder
         ::default()
-        .invoke_handler(tauri::generate_handler![greet, read_img_test, upload_img_test])
+        .invoke_handler(tauri::generate_handler![upload_img])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
