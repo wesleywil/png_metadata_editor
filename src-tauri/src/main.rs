1c6a8df72a45dd85e9702fa6592565dc544fb0f6
wesleywil <wesleywilsonti@gmail.com>
2024-02-13 11:28:05 -0300

    updated main.rs by adding a new test function that enables users to modify the tEXt of png images, for now just testing

--- src-tauri/src/main.rs
+++ src-tauri/src/main.rs
@@ -1,9 +1,14 @@
 // Prevents additional console window on Windows in release, DO NOT REMOVE!!
 #![cfg_attr(not(debug_assertions), windows_subsystem = "windows")]
 
+use std::path::Path;
+use std::fs::{ self, File };
+// use std::io::{ BufWriter, Error, Read, Write };
+
 #[derive(Debug, thiserror::Error)]
 enum Error {
-    #[error(transparent)] Png(#[from] png::DecodingError),
+    #[error(transparent)] PngDecoding(#[from] png::DecodingError),
+    #[error(transparent)] PngEncoding(#[from] png::EncodingError),
     #[error(transparent)] Io(#[from] std::io::Error),
 }
 
@@ -33,10 +38,39 @@ fn upload_img(file: String) -> Result<Vec<String>, Error> {
     Ok(formatted_strings)
 }
 
+#[tauri::command]
+fn png_metadata_edit() -> Result<(), Error> {
+    let file_path =
+        "C:/Users/wesle/OneDrive/Área de Trabalho/Projects/edit_png/src-tauri/src/test_img/to_test.png";
+    let decoder = png::Decoder::new(File::open(file_path).unwrap());
+    let mut reader = decoder.read_info().unwrap();
+    let mut buf = vec![0; reader.output_buffer_size()];
+    let info = reader.next_frame(&mut buf).unwrap();
+    let bytes = &buf[..info.buffer_size()];
+    let png_info = reader.info();
+
+    // Encode
+    let path_out = Path::new(
+        "C:/Users/wesle/OneDrive/Área de Trabalho/Projects/edit_png/src-tauri/src/test_img/to_test_edited.png"
+    );
+    let file = File::create(path_out)?;
+    let ref mut w = std::io::BufWriter::new(file);
+
+    let mut encoder = png::Encoder::new(w, png_info.width, png_info.height);
+    encoder.set_color(png_info.color_type);
+    encoder.set_depth(png_info.bit_depth);
+    encoder.add_text_chunk("parameters".to_string(), "FX".to_string());
+    encoder.add_text_chunk("data_generation".to_string(), "F".to_string());
+    let mut writer = encoder.write_header().unwrap();
+    writer.write_image_data(bytes).unwrap();
+
+    Ok(())
+}
+
 fn main() {
     tauri::Builder
         ::default()
-        .invoke_handler(tauri::generate_handler![upload_img])
+        .invoke_handler(tauri::generate_handler![upload_img, png_metadata_edit])
         .run(tauri::generate_context!())
         .expect("error while running tauri application");
 }
