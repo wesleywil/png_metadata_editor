ce69bf17e76123f34784e220c8b0eabf08729e06
wesleywil <wesleywilsonti@gmail.com>
2024-02-15 08:48:08 -0300

    updated scripts.js by adding a logic that will change the proportions of the image_container class depending on the proportions of the selected image. Added a redirecting for the user after the metadata of the image is updated and updated styles.css by adding the Done page classes and made some other small css adjustments

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -6,17 +6,19 @@ const { open } = window.__TAURI__.dialog;
 document.addEventListener("DOMContentLoaded", function () {
     // Show uploaded image
     const imgContainer = document.getElementById("img_container");
-    const imgTitle = document.getElementById("title");
     const imgParams = document.getElementById("text_params");
     const dataParams = document.getElementById("generation_data_params");
     const uploadImage = document.getElementById("btn_upload_img");
     const btnUpdate = document.getElementById("btn_update");
+    btnUpdate.classList.add("hidden")
     let openFile = "";
 
     uploadImage.addEventListener("click", async () => {
         const removeImg = document.getElementById("img_upload_ig");
         if (removeImg) {
             removeImg.remove();
+            imgContainer.classList.remove("landscape");
+            imgContainer.classList.remove("square")
         }
 
         try {
@@ -27,13 +29,20 @@ document.addEventListener("DOMContentLoaded", function () {
                     extensions: ['png']
                 }]
             });
+            btnUpdate.classList.remove("hidden")
             const res = await invoke("upload_img", { file: openFile });
             const img = document.createElement("img");
             img.id = "img_upload_ig";
             img.src = convertFileSrc(openFile)
             img.className = "img_upload";
+            img.onload = function () {
+                if (img.naturalWidth > img.naturalHeight) {
+                    imgContainer.classList.add("landscape");
+                } else if (img.naturalWidth === img.naturalHeight) {
+                    imgContainer.classList.add("square")
+                }
+            }
             imgContainer.appendChild(img);
-            imgTitle.value = res[0].replace(/^.*[\\\/]/, '');
             imgParams.value = res[1];
             dataParams.value = res[2];
 
@@ -42,14 +51,14 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Only for test
+    // Edit Png metadata
     btnUpdate.addEventListener("click", () => {
-        console.log("Parameters: ", imgParams.value);
-        console.log("Data Parameters: ", dataParams.value);
         const test_decode_encode = async () => {
             try {
-
                 const test = await invoke("png_metadata_edit", { parameters: imgParams.value, dataGeneration: dataParams.value, filePath: openFile });
+                if (test === null) {
+                    window.location.href = "done.html"
+                }
                 console.log("Result ===> ", test);
             } catch (err) {
                 console.log("Error in Decode/Encode ===> ", err);
@@ -58,8 +67,4 @@ document.addEventListener("DOMContentLoaded", function () {
 
         test_decode_encode()
     })
-
-
-
-
 });
