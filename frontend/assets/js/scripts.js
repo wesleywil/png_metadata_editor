b89edc15c1ea8e100b912ce3d75dab8d7723e3fb
wesleywil <wesleywilsonti@gmail.com>
2024-02-16 08:51:53 -0300

    updated homepage and scripts.js by adding a checkbox button that allows users to include or exclude the data_parameters in the image

--- frontend/assets/js/scripts.js
+++ frontend/assets/js/scripts.js
@@ -7,18 +7,25 @@ document.addEventListener("DOMContentLoaded", function () {
     // Show uploaded image
     const imgContainer = document.getElementById("img_container");
     const imgParams = document.getElementById("text_params");
+    const containerExtraData = document.getElementById("extra_data");
     const dataParams = document.getElementById("generation_data_params");
     const uploadImage = document.getElementById("btn_upload_img");
     const btnUpdate = document.getElementById("btn_update");
+    let btnRemoveExtra = document.getElementById("remove_extra_data");
+    let extraData = false;
+
     btnUpdate.classList.add("hidden")
     let openFile = "";
 
+    // Function allowing users to upload a image and display the metadata
     uploadImage.addEventListener("click", async () => {
         const removeImg = document.getElementById("img_upload_ig");
         if (removeImg) {
             removeImg.remove();
             imgContainer.classList.remove("landscape");
             imgContainer.classList.remove("square")
+            btnRemoveExtra.checked = false
+            containerExtraData.hidden = false
         }
 
         try {
@@ -42,8 +49,14 @@ document.addEventListener("DOMContentLoaded", function () {
                     imgContainer.classList.add("square")
                 }
             }
+            console.log('Test extraData ==> ', res[2]);
             imgContainer.appendChild(img);
             imgParams.value = res[1];
+            if (res[2] === "no parameters extra data") {
+                containerExtraData.hidden = true
+                btnRemoveExtra.checked = true
+
+            }
             dataParams.value = res[2];
 
         } catch (err) {
@@ -51,20 +64,30 @@ document.addEventListener("DOMContentLoaded", function () {
         }
     });
 
-    // Edit Png metadata
+    // Function allowing users to edit the metadata of a image
     btnUpdate.addEventListener("click", () => {
         const test_decode_encode = async () => {
             try {
-                const test = await invoke("png_metadata_edit", { parameters: imgParams.value, dataGeneration: dataParams.value, filePath: openFile });
+                const test = await invoke("png_metadata_edit", { parameters: imgParams.value, dataGeneration: dataParams.value, noExtraData: extraData, filePath: openFile });
                 if (test === null) {
                     window.location.href = "done.html"
                 }
-                console.log("Result ===> ", test);
             } catch (err) {
                 console.log("Error in Decode/Encode ===> ", err);
             }
         }
 
         test_decode_encode()
+    })
+
+    btnRemoveExtra.addEventListener("change", function () {
+        if (this.checked) {
+            extraData = this.checked;
+            containerExtraData.hidden = true
+
+        } else {
+            extraData = this.checked;
+            containerExtraData.hidden = false
+        }
     })
 });
